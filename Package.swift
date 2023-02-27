11b4d979cf8888bd49be004769bae1c0767756cf
Guy Shaviv <guy@tivstudio.com>
2023-02-27 09:10:33 +0200

    v12

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TeslaBackendAPI",
     platforms: [
-      .macOS(.v13),
+      .macOS(.v12),
     ],
     products: [
         .library(
