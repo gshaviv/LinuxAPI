f123581b1a428acf8c1a0ddbafd1eff3694870e5
Guy Shaviv <guy@shaviv.org>
2023-12-20 16:26:14 +0200

    fix delimeter

--- Sources/TeslaBackendAPI/API.swift
+++ Sources/TeslaBackendAPI/API.swift
@@ -330,7 +330,7 @@ extension Tesla {
   }
   
   public static func exchange(code: String) async throws -> AuthToken {
-    let request = GenerateTokenRequest(code: code, kind: RefreshTokenRequest.fleetAPI, audience: [API.Region.northAmeria, API.Region.europe].map { $0.host }.joined(separator: "%20"))
+    let request = GenerateTokenRequest(code: code, kind: RefreshTokenRequest.fleetAPI, audience: [API.Region.northAmeria, API.Region.europe].map { $0.host }.joined(separator: " "))
     return try await API.call(host: API.authHost, endpoint: API.authEndpoint, body: request, token: { nil }, onTokenRefresh: nil)
   }
 }
